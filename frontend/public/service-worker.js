b2df30c6079d37afabcd0ffdcca925e53def27d3
Maera <baeitsmae@gmail.com>
2024-11-24 10:24:43 +0100

    base project test

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,14 @@
+
+// Fichier public/service-worker.js
+
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    event.waitUntil(
+      self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: data.icon,
+      })
+    );
+});
+  
+  
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+
+    const subscribeToPush = async () => {
+        if ('serviceWorker' in navigator && 'PushManager' in window) {
+             // Vérifier si un abonnement existe déjà
+             const registration = await navigator.serviceWorker.ready;
+        const existingSubscription = await registration.pushManager.getSubscription();
+        
+        if (existingSubscription) {
+            // Se désabonner de l'ancien abonnement
+            await existingSubscription.unsubscribe();
+        }
+
+        // Obtenir la clé publique du serveur
+        const response = await fetch('http://localhost:5000/vapidPublicKey');
+        const { publicKey } = await response.json();
+        
+        // S'abonner à la nouvelle clé publique
+        const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: publicKey,
+        });
+        
+        // Enregistrer le nouvel abonnement
+        await fetch('http://localhost:5000/subscribe', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(subscription),
+        });
+        
+            setSuccess('Notifications activées avec succès !');
+        } 
+        else {
+          setError('Votre navigateur ne supporte pas les notifications push.');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Hello Gorgeous!</h1>
+            <p>Active les notifications pour recevoir ton rappel quotidien.</p>
+            <button onClick={subscribeToPush}>Activer les notifications</button>
+            {error && <p style={{color: "red"}}>{error}</p>}
+            {success && <p style={{color: "green"}}>{success}</p>}
+        </div>
+    )
 }
 
-export default App;
+export default App
